51f3a326aee9be59e9d0fd5eb18b1926b279f9ed
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-10 22:23:32 +0300

    Initial commit

---
+++ FourOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿class FourOperations
+{
+    static void Main(string[] args)
+
+    {
+        double firstNumber = double.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+
+        double additionSum = firstNumber + secondNumber;
+        double subtractionSum = firstNumber - secondNumber;
+        double multiplicationSum = firstNumber * secondNumber;
+        double divisionSum = firstNumber / secondNumber;
+        
+        Console.WriteLine($"{firstNumber:F2} + {secondNumber:F2} = {firstNumber+secondNumber:F2}");
+        Console.WriteLine($"{firstNumber:F2} - {secondNumber:F2} = {firstNumber-secondNumber:F2}");
+        Console.WriteLine($"{firstNumber:F2} * {secondNumber:F2} = {firstNumber*secondNumber:F2}");
+        Console.WriteLine($"{firstNumber:F2} / {secondNumber:F2} = {firstNumber/secondNumber:F2}");
+        
+
+    }
+}
